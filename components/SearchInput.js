5d198497368c139e173a5fd00160783a7a0616b4
Cernan Bernardo <cernanb@gmail.com>
2017-12-09 23:03:26 -0800

    Add local component state and call to prop method to update city

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,20 +1,41 @@
-import React from "react"
-import { StyleSheet, View, TextInput } from "react-native"
+import React from 'react'
+import { StyleSheet, View, TextInput } from 'react-native'
 
 export default class SearchInput extends React.Component {
-  handleChangeText(newLocation) {}
+  state = {
+    text: ''
+  }
+  handleChangeText = text => {
+    this.setState({
+      text
+    })
+  }
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props
+    const { text } = this.state
+
+    if (!text) return
+
+    onSubmit(text)
+    this.setState({ text: '' })
+  }
 
   render() {
+    const { text } = this.state
+    const { placeholder } = this.props
     return (
       <View style={styles.container}>
         <TextInput
           autoCorrect={false}
-          placeholder={this.props.placeholder}
+          placeholder={placeholder}
           placeholderTextColor="white"
           underlineColorAndroid="transparent"
           style={styles.textInput}
           clearButtonMode="always"
           onChangeText={this.handleChangeText}
+          value={text}
+          onSubmitEditing={this.handleSubmitEditing}
         />
       </View>
     )
@@ -25,13 +46,13 @@ const styles = StyleSheet.create({
   container: {
     height: 40,
     marginTop: 20,
-    backgroundColor: "#666",
+    backgroundColor: '#666',
     marginHorizontal: 40,
     paddingHorizontal: 10,
     borderRadius: 5
   },
   textInput: {
     flex: 1,
-    color: "white"
+    color: 'white'
   }
 })
